23379c87aa7ff4347722108f2205b19175a09138
MatejVukosav <bitbucketking>
2017-01-11 00:22:45 +0100

    Add project files.

---
+++ MongoNewsWebAplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MongoNewsWebAplication.Startup))]
+namespace MongoNewsWebAplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
